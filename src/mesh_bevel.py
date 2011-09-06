30a96f1db75a6bd53606a78aa22fcd8d6d99d67a
neomonkeus <abominalmonkey@gmail.com>
2011-09-06 21:05:53 +0100

    Added operator to Specials (W-Key) Menu. Tidied Object/Edit mode switching.

--- src/mesh_bevel.py
+++ src/mesh_bevel.py
@@ -16,16 +16,47 @@ bl_info = {
 import bpy
 from bpy.utils import register_module, unregister_module
 
-#=== ###Operator
+'''  
+Type declaration: useful for auto-completion 
+Comment before publishing
+'''
+    #edge = bpy.types.MeshEdge
+    #obj = bpy.types.Object
+    #bevel = bpy.types.BevelModifier
+
+#--- ###Operator
 class Bevel(bpy.types.Operator):
     '''Bevels selected edges of the mesh'''
     bl_idname = "mesh.bevel"
     bl_label = "Bevel"
     bl_description = "Bevel selected edges"
     
+    @classmethod
+    def poll(cls, context):
+        return(context.mode == "EDIT_MESH")
+    
     def execute(self, context):
         bevel(context.active_object, 0.1)
+        bpy.ops.object.editmode_toggle()
         return('FINISHED')
+    
+    def invoke(self, context, event):
+        #refresh data:
+        bpy.ops.object.editmode_toggle()
+        
+        selected = 0
+        
+        #TODO: compare with active obj implementation
+        for edge in context.object.data.edges:
+            if edge.select:
+                selected += 1
+                
+        if selected > 0:
+            return self.execute(context)
+        else:
+            bpy.ops.object.editmode_toggle()
+            self.report(type = "ERROR", message = "No edges selected")
+            return {'CANCELLED'}
     
 def bevel(obj, width):
     """Bevels selected edges of the mesh
@@ -34,12 +65,7 @@ def bevel(obj, width):
             It should have some edges selected
             This function should only be called in Edit Mode.
     """
-
-    #edge = bpy.types.MeshEdge
-    #obj = bpy.types.Object
-    #bevel = bpy.types.BevelModifier
-
-    bpy.ops.object.mode_set(mode='OBJECT',toggle=False)
+    
     #add Bevel modifier
     bpy.ops.object.modifier_add(type='BEVEL')
     
@@ -63,14 +89,18 @@ def bevel(obj, width):
             edge.bevel_weight = 0.0
     
     bpy.ops.object.editmode_toggle()
-    #bpy.ops.object.mode_set(mode='EDIT', toggle=False)
-    print(obj)
+
+def menu_draw(self, context):
+    self.layout.operator_context = 'INVOKE_REGION_WIN'
+    self.layout.operator(Bevel.bl_idname, "Bevel")
             
 def register():
     register_module(__name__)
+    bpy.types.VIEW3D_MT_edit_mesh_specials.prepend(menu_draw)
     
 def unregister():
     unregister_module(__name__)
+    bpy.types.VIEW3D_MT_edit_mesh_specials.remove(menu_draw)
     
 if __name__ == '__main__':
     register()
